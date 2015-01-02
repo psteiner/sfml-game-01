4b74ad6b7552753fc63a205b5ab973d5e2415f74
Philip Steiner <philip@psteiner.com>
2015-01-01 21:28:12 -0800

    Add Entity base class

---
+++ SFML Game 01/Entity.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <SFML/System.hpp>
+
+class Entity
+{
+public:
+    void            setVelocity(sf::Vector2f velocity);
+    void            setVelocity(float vx, float vy);
+    sf::Vector2f    getVelocity() const;
+
+private:
+    sf::Vector2f    mVelocity;
+};
+
